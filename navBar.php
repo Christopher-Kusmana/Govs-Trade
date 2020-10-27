9f30ba927685edc7bcdb97181348da205803e6d0
ChrisForaste <chris.foraste@gmail.com>
2020-10-27 18:17:23 -0400

    Added Separate Navigation php and css files

---
+++ navBar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homeStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "gridContainer">
+        <div id = "links">
+            <a>Portfolio</a>
+            <a>Marketplace</a>
+            <a>Leaderboard</a>
+            <a>Your Friends</a>
+            <a>Contact Us</a>
+        </div>
+    </div>
+</body>
+</html>
