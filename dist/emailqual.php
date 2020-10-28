ac0d8ad7922db692e60af7e81c58e1302a76ca90
Christopher-Kusmana <christopherkusmanateg@gmail.com>
2020-10-27 22:15:46 -0400

    Email Qualification System

---
+++ dist/emailqual.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="includes/emailqual.inc.php" method="POST">
+    Email: <input type="email" id="email" name="email" required>
+    <input type="submit" name="email-submit" value="Register">
+    </form>
+</body>
+</html>
